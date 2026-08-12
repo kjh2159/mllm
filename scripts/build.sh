d82e35953d6cf6a6ff6b881ececf41975e168af0
kjh2159 <kjh2159@dgist.ac.kr>
2026-08-12 11:27:35 +0900

    update: default build option

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,11 @@
 mkdir ../build
 cd ../build || exit
 
-cmake .. -DIGNITE_USE_SYSTEM=ON -DPERFETTO=OFF -DMV=OFF -DCMAKE_BUILD_TYPE=Release
+cmake ..\
+	-DIGNITE_USE_SYSTEM=ON \
+	-DPERFETTO=OFF \
+	-DMV=OFF \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_FLAGS="-D__ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__"
 
 make -j$(nproc)
